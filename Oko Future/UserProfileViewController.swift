160dc761d10e8c2bda14e4a7f384074df9445459
Денис Калинин <deniskalinin@iMac-Denis.local>
2023-06-14 15:53:23 +0400

    add log out logic

--- Oko Future/UserProfileViewController.swift
+++ Oko Future/UserProfileViewController.swift
@@ -15,18 +15,33 @@ final class UserProfileViewController: UIViewController {
         return btn
     }()
     
+    let uploadBackView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray
+        return view
+    }()
+    
+    let uploadImageView: UIImageView = {
+        let img = UIImage(named: "downloadArrow")
+        let imgV = UIImageView(image: img)
+        imgV.contentMode = .scaleAspectFill
+        return imgV
+    }()
+    
     private let nameUserLabel: UILabel = {
         let lbl = UILabel()
+        lbl.textAlignment = .center
+        lbl.textColor = .white
         return lbl
     }()
     
-    private let logOutButton: UIButton = {
-       let btn = UIButton()
+    private let logOutButton: UIOkoProfileSettingButton = {
+        let btn = UIOkoProfileSettingButton(size: .big)
         return btn
     }()
     
-    private let deleteUserButton: UIButton = {
-       let btn = UIButton()
+    private let deleteUserButton: UIOkoProfileSettingButton = {
+        let btn = UIOkoProfileSettingButton(size: .big)
         return btn
     }()
     
@@ -39,23 +54,50 @@ final class UserProfileViewController: UIViewController {
     }
     
     private func setupView() {
+        view.backgroundColor = .white.withAlphaComponent(0.72)
+        
         view.addSubview(backButton)
+        view.addSubview(uploadBackView)
+        view.addSubview(uploadImageView)
         view.addSubview(nameUserLabel)
         view.addSubview(logOutButton)
         view.addSubview(deleteUserButton)
         
-        let user = Helper().getUser()!
-        nameUserLabel.text = user.name
+        guard let user = Helper().getUser() else {
+            navigationController?.popViewController(animated: true)
+            return
+        }
+        
+        if user.name == nil {
+            nameUserLabel.text = "username unknown"
+        } else {
+            nameUserLabel.text = user.name
+        }
         
         backButton.addTarget(self, action: #selector(backButtonTap), for: .touchUpInside)
-        logOutButton.addTarget(self, action: #selector(logOutButtonTap), for: .touchUpInside)
-        deleteUserButton.addTarget(self, action: #selector(deleteUserButtonTap), for: .touchUpInside)
+        
+        let logOutTapGesture = UITapGestureRecognizer(target: self, action: #selector(logOutButtonTap))
+        let deleteUserTapGesture = UITapGestureRecognizer(target: deleteUserButton, action: #selector(deleteUserButtonTap))
+        
+        logOutButton.configureView(image: UIImage(), text: "Log out", tapGesture: logOutTapGesture)
+        deleteUserButton.configureView(image: UIImage(), text: "Delete Account", tapGesture: deleteUserTapGesture)
     }
     
     private func setupLayout() {
         let heightOko: CGFloat = 48
         
         backButton.frame = CGRect(x: 21, y: 61, width: heightOko, height: heightOko)
+        
+        uploadBackView.frame = CGRect(x: (view.bounds.width - 160)/2, y: 130, width: 160, height: 160)
+        uploadBackView.layer.cornerRadius = uploadBackView.bounds.size.height / 2.0
+        
+        uploadImageView.frame.size = CGSize(width: 20, height: 20)
+        uploadImageView.center = uploadBackView.center
+        
+        nameUserLabel.frame = CGRect(x: 0, y: uploadBackView.frame.origin.y + 160 + 22, width: view.bounds.width, height: 50)
+        
+        logOutButton.frame.origin = CGPoint(x: (view.frame.width - logOutButton.frame.width) / 2, y: 400)
+        deleteUserButton.frame.origin = CGPoint(x: (view.frame.width - deleteUserButton.frame.width) / 2, y: 500)
     }
     
     @objc func backButtonTap() {
@@ -63,11 +105,21 @@ final class UserProfileViewController: UIViewController {
     }
     
     @objc func logOutButtonTap() {
-        Helper().logOut()
+        guard let navigationController = self.navigationController else { return }
+//        Helper().logOut()
+        if let welcomeVc = navigationController.viewControllers[0] as? WelcomeViewController {
+            navigationController.popToViewController(welcomeVc, animated: true)
+        } else {
+            let startVc = WelcomeViewController()
+            navigationController.viewControllers.removeAll()
+            navigationController.pushViewController(startVc, animated: true)
+        }
+        
     }
     
     @objc func deleteUserButtonTap() {
-        Helper().deleteUserFirebase()
+//        Helper().deleteUserFirebase()
+        logOutButtonTap()
     }
     
 }
